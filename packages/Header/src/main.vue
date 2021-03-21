9483721f763ef7a22c48c48dea91ecb389439a9a
pengmeng <1575170534@qq.com>
2021-03-21 15:34:53 +0800

    header.vue

---
+++ packages/Header/src/main.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+     <div class="title">{{title}}</div>
+     <div slot="desc"></div>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        title:{
+            type:String
+        }
+    }
+}
+</script>
